b4edd2af55136d09ad5582541c420cea833d6c4d
muskaan-0908 <muskaanmakkar987@gmail.com>
2026-02-22 23:46:02 +0530

    Delete run_project.bat

--- run_project.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Starting Gesture Control System...
-cd backend
-pip install fastapi uvicorn opencv-python mediapipe joblib scikit-learn pyautogui
-start http://127.0.0.1:8000
-python -m uvicorn main:app --reload
-pause
